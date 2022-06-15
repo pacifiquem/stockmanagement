6d3b5bd218767da3c3c7f47fbb7b144826313c2e
pacy001 <pacifiquemurangwa001@gmail.com>
2022-06-15 20:24:03 +0200

    finalized signup and login functionalities at backend

--- backend-files/user_crud.php
+++ backend-files/user_crud.php
@@ -1,7 +1,8 @@
 <?php
 
 include('./db_connection.php');
-class signup extends Db_Connection {
+
+class User {
 
     private $userId;
     private $firstName;
@@ -14,7 +15,7 @@ class signup extends Db_Connection {
     private $password;
     private $connection;
 
-    function __construct($firstName, $lastName, $telephone, $gender, $nationality, $username, $email, $password, $connection) {
+    public function setSignUp_info($firstName, $lastName, $telephone, $gender, $nationality, $username, $email, $password, $connection) {
 
         $this->userId = '';
         $this->firstName = $firstName;
@@ -27,6 +28,15 @@ class signup extends Db_Connection {
         $this->password = $password;
         $this->connection = $connection;
         $this->password = $password;
+
+    }
+
+    public function setLogIn_info($email, $password, $connection) {
+
+        $this->email = $email;
+        $this->password = $password;
+        $this->connection = $connection;
+
     }
 
     public function hashPassword() {
@@ -46,20 +56,61 @@ class signup extends Db_Connection {
         $query = mysqli_query($this->connection, $insert);
 
         if($query) {
-            // //store user information in session
-            // session_start();
-            // $_SESSION['user'] = $this->id;
-            // //direct user to his dashboard
-            // header('Location: ../front-files/dashboard.php');
+            //store user information in session
+            session_start();
+            $_SESSION['user'] = $this->id;
+            //direct user to his dashboard
+            header('Location: ../front-files/dashboard.php');
         }else {
             echo mysqli_error($this->connection);
             header('Location: ../front-files/signup.php?error=please fillout all fields wisely');
         }
     }
+
+
+    public function logIn() {
+
+        $select = "SELECT * FROM USERS where email='$this->email'";
+        $query = mysqli_query($this->connection, $select);
+
+        if($query && ($query->num_rows != 0)) {
+
+
+            $user = mysqli_fetch_assoc($query);
+            if(hash('sha256',$this->password) === $user['password']) {
+                //starting session
+                session_start();
+
+                $_SESSION['userId'] = $this->userId;
+                $_SESSION['email'] = $this->email;
+                $_SESSION['username'] = $this->username;
+
+                header("Location: ../front-files/dashboard.php");
+            }
+        }
+    }
 }
-$newUser = new signup($_POST['firstname'], $_POST['lastname'], $_POST['telephone'], $_POST['gender'], $_POST['nationality'], $_POST['username'], $_POST['email'], $_POST['password'], $dbConnection);
-$newUser->hashPassword();
-$newUser->generateId();
-$newUser->signUp();
+
+
+if(isset($_GET['signup']) && $_GET['signup'] == TRUE) {
+
+    $newUser = new User();
+
+    //setting required fields
+    $newUser->setSignUp_info($_POST['firstname'], $_POST['lastname'], $_POST['telephone'], $_POST['gender'], $_POST['nationality'], $_POST['username'], $_POST['email'], $_POST['password'], $dbConnection);
+
+    //inserting user into db with complete information.
+    $newUser->hashPassword();
+    $newUser->generateId();
+    $newUser->signUp();
+}
+
+if(isset($_GET['login']) && $_GET['login'] == TRUE) {
+
+    $userLoggedIn = new User();
+    $userLoggedIn->setLogIn_info($_POST['email'], $_POST['password'], $dbConnection);
+    $userLoggedIn-> logIn();
+
+}
 
 ?>
