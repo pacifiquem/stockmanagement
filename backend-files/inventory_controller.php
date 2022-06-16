78cb397dd97e5051d6876b642db946c21d202cba
pacy001 <pacifiquemurangwa001@gmail.com>
2022-06-16 08:25:46 +0200

    delete invetory functionalities added

--- backend-files/inventory_controller.php
+++ backend-files/inventory_controller.php
@@ -9,23 +9,43 @@
         private $productId;
         private $connection;
 
-        function setAddInventory_info($quantity, $productId, $connection) {
+        public function setAddInventory_info($quantity, $productId, $connection) {
             $this->quantity = $quantity;
             $this->productId = $productId;
             $this->connection = $connection;
+        }
+
+        public function setDeleteInventory_info($inventoryId, $connection) {
+
+            $this->inventoryId = $inventoryId;
+            $this->connection = $connection;
+
         }
 
         public function generateId() {
             $this->productId = uniqid();
         }
 
-        function addInventory() {
+        public function addInventory() {
 
-            $insert = "INSERT INTO stk_inventory(inventory_id, quantity, productId) VALUES('$this->inventoryId', '$this->quantity, $this->productId')";
+            $insert = "INSERT INTO stk_inventory(inventory_id, quantity, productId) VALUES('$this->inventoryId', '$this->quantity', $this->productId')";
             $query = mysqli_query($this->connection, $insert);
 
             if($query && ($query->num_rows != 0)) {
                 header("Location: ../front-files/dashboard.php?message=New stock inventory added");
+            }
+        }
+
+
+        public function deleteInventory() {
+
+            $delete = "DELETE FROM stk_inventory WHERE inventory_id = '$this->inventoryId'";
+            $query = mysqli_query($this->connection, $delete);
+
+            if($query) {
+                header("Location ../front-files/dashboard.php?message=Inventory ($this->inventoryId) Deleted Successfully");
+            }else {
+                header("Location ../front-files/dashboard.php?message=Unable to delete Inventory with id ($this->inventoryId);");
             }
         }
 
@@ -36,6 +56,14 @@
         $newInventory->setAddInventory_info($_GET['quantity'], $_GET['productId'], $dbConnection);
         $newInventory->generateId();
         $newInventory->addInventory();
+    }
+
+    if(isset($_GET['deleteinventory']) && ($_GET['deleteinventory'] == TRUE)) {
+
+        $deleteInventory = new Inventory;
+        $deleteInventory->setDeleteInventory_info($_GET['inventoryId'], $dbConnection);
+        $deleteInventory->deleteInventory();
+
     }
 
 ?>
