1f903378594d880e183406c67d7a6cf908401824
pacy001 <pacifiquemurangwa001@gmail.com>
2022-06-16 08:10:16 +0200

    add new inventory functionalities added

--- backend-files/inventory_controller.php
+++ backend-files/inventory_controller.php
@@ -1,3 +1,41 @@
 <?php
 
+    include('db_connection.php');
+
+    class Inventory {
+
+        private $inventoryId;
+        private $quantity;
+        private $productId;
+        private $connection;
+
+        function setAddInventory_info($quantity, $productId, $connection) {
+            $this->quantity = $quantity;
+            $this->productId = $productId;
+            $this->connection = $connection;
+        }
+
+        public function generateId() {
+            $this->productId = uniqid();
+        }
+
+        function addInventory() {
+
+            $insert = "INSERT INTO stk_inventory(inventory_id, quantity, productId) VALUES('$this->inventoryId', '$this->quantity, $this->productId')";
+            $query = mysqli_query($this->connection, $insert);
+
+            if($query && ($query->num_rows != 0)) {
+                header("Location: ../front-files/dashboard.php?message=New stock inventory added");
+            }
+        }
+
+    }
+
+    if(isset($_GET['addinventory']) && ($_GET['addinventory'] == TRUE)) {
+        $newInventory = new Inventory();
+        $newInventory->setAddInventory_info($_GET['quantity'], $_GET['productId'], $dbConnection);
+        $newInventory->generateId();
+        $newInventory->addInventory();
+    }
+
 ?>
